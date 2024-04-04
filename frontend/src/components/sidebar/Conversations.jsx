2d8494b5c0a55f867e0831600bef431adf20bf1f
Kundan Kumar <kundanrajpoot4@gmail.com>
2024-04-04 11:52:51 +0530

    completed 2nd part

---
+++ frontend/src/components/sidebar/Conversations.jsx
@@ -0,0 +1,38 @@
+import Conversation from "./Conversation";
+
+
+const Conversations = () => {
+  return (
+      <div className="py-2 flex flex-col overflow-auto">
+          <Conversation />
+          <Conversation />
+          <Conversation />
+          <Conversation />
+          <Conversation />
+          <Conversation />
+      
+    </div>
+  )
+}
+
+export default Conversations;
+
+
+// import Conversation from "./Conversation";
+
+
+// const Conversations = () => {
+//   return (
+//       <div className="py-2 flex flex-col overflow-auto">
+//           <Conversation />
+//           <Conversation />
+//           <Conversation />
+//           <Conversation />
+//           <Conversation />
+//           <Conversation />
+      
+//     </div>
+//   )
+// }
+
+// export default Conversations;
